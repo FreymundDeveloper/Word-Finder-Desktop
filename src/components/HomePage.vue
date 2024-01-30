09154810a6ea7da4083ac64f637c4314578114bc
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-29 22:20:45 -0300

    File Attachment Functionality

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="pills">
-    <PillComponent v-for="item in groupedWords" :word="item.word" 
-    :amount="item.amount" :key="item.word" />
-  </div>
+    <div class="home">
+        <v-form>
+            <v-file-input label="Select some file" prepend-icon="mdi-message-text"
+            append-icon="mdi-send" outlined multiple v-model="files" @click:append="processSubtitles">
+
+            </v-file-input>
+        </v-form>
+
+        <div class="pills">
+            <PillComponent v-for="item in groupedWords" :word="item.word" 
+            :amount="item.amount" :key="item.word" />
+        </div>
+    </div>
 </template>
 
 <script>
@@ -14,18 +23,30 @@ export default {
     },
     data: function() {
         return {
+            files: [],
             groupedWords: [
                 {word: "i", amount: 547},
                 {word: "you", amount: 478},
                 {word: "it", amount: 10}
             ]
         }
+    },
+    methods: {
+        processSubtitles() {
+            console.log(this.files)
+        }
     }
 }
 </script>
 
 <style scoped>
+    .home {
+        margin: 20px;
+    }
+
     .pills {
         display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
     }
 </style>
