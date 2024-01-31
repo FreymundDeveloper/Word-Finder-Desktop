62f6ba0449d1f8274169060dbf1392a90965e59d
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-01-31 00:55:08 -0300

    Words Group Builder Process

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,17 +1,14 @@
 const {ipcMain} = require("electron");
 const pathsToRows = require("./pathsToRows");
 const prepareData = require("./prepareData");
+const groupWords = require("./groupWords");
 
 ipcMain.on("process-subtitles", (event, paths) => {
     console.log(paths)
     pathsToRows(paths)
     .then(rows => prepareData(rows))
-    .then(prepareData => console.log(prepareData))
-    .then(() => {
-        event.reply("process-subtitles", [
-            { word: "i", amount: 547 },
-            { word: "you", amount: 478 },
-            { word: "it", amount: 10 },
-        ]);
+    .then(prepareData => groupWords(prepareData))
+    .then(groupWords => {
+        event.reply("process-subtitles", groupWords);
     })
 })
